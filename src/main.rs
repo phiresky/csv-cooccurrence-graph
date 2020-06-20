64a675ad756e55fe80acc88e4c09d95076158ff6
phiresky <phireskyde+git@gmail.com>
2020-06-20 10:42:14 +0200

    noiceee

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use csv::StringRecord;
-use fnv::FnvHashMap;
+use fnv::{FnvBuildHasher, FnvHashMap};
+use indexmap::IndexMap;
 use indicatif::{ProgressBar, ProgressBarWrap, ProgressStyle};
+use itertools::Itertools;
+use std::fs::File;
 use std::io::{BufReader, Write};
-use std::{collections::HashMap, fs::File};
+
+type CsvReader = csv::Reader<BufReader<ProgressBarWrap<File>>>;
+
+fn open_csv(path: &str) -> Result<CsvReader> {
+    let inp = File::open(path)?;
+    let progress_style = ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta_precise})");
+    let bar = ProgressBar::new(inp.metadata()?.len()).with_style(progress_style);
+    Ok(csv::Reader::from_reader(BufReader::with_capacity(
+        10_000_000,
+        bar.wrap_read(inp),
+    )))
+}
 
 fn get_metaexpressions<'a>(record: &'a StringRecord) -> impl Iterator<Item = &'a str> {
     // let tweet_year = &record[0];
@@ -21,33 +36,17 @@ fn get_metaexpressions<'a>(record: &'a StringRecord) -> impl Iterator<Item = &'a
 const min_to_retain_nodes: i64 = 10;
 const min_to_retain_edges: i64 = 3;
 
-type CsvReader = csv::Reader<BufReader<ProgressBarWrap<File>>>;
+fn read_nodes(mut csv: CsvReader) -> Result<IndexMap<String, i64, FnvBuildHasher>> {
+    let mut nodes = IndexMap::with_capacity_and_hasher(1000_000, FnvBuildHasher::default());
 
-fn open_csv(path: &str) -> Result<CsvReader> {
-    let inp = File::open(path)?;
-    let progress_style = ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta_precise})");
-    let bar = ProgressBar::new(inp.metadata()?.len()).with_style(progress_style);
-    Ok(csv::Reader::from_reader(BufReader::with_capacity(
-        10_000_000,
-        bar.wrap_read(inp),
-    )))
-}
-
-fn read_nodes(mut csv: CsvReader) -> Result<FnvHashMap<String, i64>> {
-    let mut nodes: HashMap<String, i64, _> =
-        FnvHashMap::with_capacity_and_hasher(1000_000, Default::default());
-
-    //let bar = ProgressBar::new((total / prog_chunk) as u64)
     let mut record = csv::StringRecord::new();
     while csv.read_record(&mut record)? {
-        let mexes = get_metaexpressions(&record);
-        for mex in mexes {
-            let r = nodes.get_mut(mex);
-            if let Some(x) = r {
-                *x += 1;
-            } else {
-                nodes.insert(mex.to_string(), 1);
+        for mex in get_metaexpressions(&record) {
+            match nodes.get_mut(mex) {
+                Some(x) => *x += 1,
+                None => {
+                    nodes.insert(mex.to_string(), 1);
+                }
             }
         }
     }
@@ -56,25 +55,23 @@ fn read_nodes(mut csv: CsvReader) -> Result<FnvHashMap<String, i64>> {
 
 fn read_edges(
     mut csv: CsvReader,
-    nodes: FnvHashMap<String, i64>,
-) -> Result<FnvHashMap<(String, String), i64>> {
-    let mut edges: HashMap<(String, String), i64, _> =
-        FnvHashMap::with_capacity_and_hasher(1000_000, Default::default());
+    nodes: &IndexMap<String, i64, FnvBuildHasher>,
+) -> Result<FnvHashMap<(usize, usize), i64>> {
+    let mut edges = FnvHashMap::with_capacity_and_hasher(1000_000, Default::default());
     let mut record = csv::StringRecord::new();
     while csv.read_record(&mut record)? {
         let mexes: Vec<_> = get_metaexpressions(&record)
-            .filter(|mex| nodes.contains_key(*mex))
+            .filter_map(|mex| nodes.get_index_of(mex))
             .collect();
-        for (inx, mut a) in mexes.iter().enumerate() {
-            for mut b in &mexes[(inx + 1)..] {
-                if a > b {
-                    std::mem::swap(&mut a, &mut b);
-                }
-                let k = (a.to_string(), b.to_string());
-                let r = edges.get_mut(&k);
-                if let Some(x) = r {
-                    *x += 1;
-                } else {
+
+        for (mut a, mut b) in mexes.iter().tuple_combinations() {
+            if a > b {
+                std::mem::swap(&mut a, &mut b);
+            }
+            let k = (*a, *b);
+            match edges.get_mut(&k) {
+                Some(x) => *x += 1,
+                None => {
                     edges.insert(k, 1);
                 }
             }
@@ -84,8 +81,12 @@ fn read_edges(
 }
 
 fn main() -> Result<()> {
-    let path = "/home/tehdog/tmp/iris/meta-expressions-100m.csv";
+    let path = std::env::args()
+        .skip(1)
+        .next()
+        .context("Supply csv as first argument")?;
     let mut nodes = read_nodes(open_csv(&path)?)?;
+    eprintln!("");
     eprintln!("total nodes: {}", nodes.len());
     nodes.retain(|_, v| *v >= min_to_retain_nodes);
     eprintln!("total nodes after filtering: {}", nodes.len());
@@ -95,15 +96,21 @@ fn main() -> Result<()> {
         writeln!(&mut f, "{}\t{}", k, v)?;
     }
 
-    let mut edges = read_edges(open_csv(&path)?, nodes)?;
+    let mut edges = read_edges(open_csv(&path)?, &nodes)?;
 
     eprintln!("total edges: {}", edges.len());
     edges.retain(|_, v| *v >= min_to_retain_edges);
     eprintln!("total edges after filtering: {}", edges.len());
 
     let mut f = File::create("edges.tsv")?;
-    for (k, v) in &edges {
-        writeln!(&mut f, "{}\t{}\t{}", k.0, k.1, v)?;
+    for (&(inx_1, inx_2), v) in &edges {
+        writeln!(
+            &mut f,
+            "{}\t{}\t{}",
+            nodes.get_index(inx_1).unwrap().0,
+            nodes.get_index(inx_2).unwrap().0,
+            v
+        )?;
     }
 
     Ok(())
